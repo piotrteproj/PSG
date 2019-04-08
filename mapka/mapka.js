32f4649af2bc7e8dd0ec6de5c2d035347e4767e1
piotrili1.github.io <45821235+piotrili@users.noreply.github.com>
2019-04-08 09:45:16 +0200

    koniec 2 zajęć
    problem z załadowaniem granic

--- mapka/mapka.js
+++ mapka/mapka.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
     var tileOSM = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
     
-    var mymap= L.map('mymap',{center: [52.3289,21.0],zoom: 10});
+    var mymap = L.map('mymap',{center: [52.3289,21.0],zoom: 10});
     mymap.addLayer(tileOSM);
+    
+    var stylGminy = {
+        "color": "green",
+    };
+    
+    var gminy = new L.GeoJSON.AJAX("gminy.geojson", {style: stylGminy});
+    gminy.addTo(mymap);
 }
 );
