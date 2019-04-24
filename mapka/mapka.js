b216803b72f966579ce173e558b440c0958f142a
piotrili1.github.io <45821235+piotrili@users.noreply.github.com>
2019-04-24 17:03:45 +0200

    koniec zajec 333
    koniec

--- mapka/mapka.js
+++ mapka/mapka.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var tileOSM = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
     
-    var mymap = L.map('mymap',{center: [52.3289,21.0],zoom: 10});
+    var mymap = L.map('mymap',{center: [54.3467, 18.8333],zoom: 10});
     mymap.addLayer(tileOSM);
     
     var stylGminy = {
@@ -10,5 +10,13 @@ $(document).ready(function(){
     
     var gminy = new L.GeoJSON.AJAX("gminy.geojson", {style: stylGminy});
     gminy.addTo(mymap);
-}
-);
+
+    var image = "port1.png",
+    imageBounds = [[54.59583, 16.86187], [54.57917, 16.845]];
+L.imageOverlay(image, imageBounds).addTo(mymap);
+    
+    var image1 = "druga.png",
+    imageBounds1 = [[54.3467, 18.8333], [54.333, 18.815]];
+L.imageOverlay(image1, imageBounds1).addTo(mymap);
+
+});
